28a6f323436b431d8d980445f5f16d2338e8a76a
lastfreeacc <last.free.acc@gmail.com>
2020-01-05 01:29:15 +0300

    add api v2

--- fgaapi/core.go
+++ fgaapi/core.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"html"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -13,10 +14,12 @@ import (
 type method string
 
 const (
-	apiURL                = "http://fucking-great-advice.ru/api/"
-	randomMth      method = "random"
-	randomHerMth   method = "random_by_tag/для%20нее"
-	randomCoderMth method = "random_by_tag/кодеру"
+	apiURL              = "http://fucking-great-advice.ru/api/"
+	randomMth    method = "random"
+	apiV2URL            = "http://fucking-great-advice.ru/api/v2/"
+	randomTagMth method = "random-advices-by-tag?tag="
+	forHerTag           = "for-her"
+	codingTag           = "coding"
 )
 
 var (
@@ -24,60 +27,114 @@ var (
 	ErrBadStatus = errors.New("Bad status")
 	// ErrEmptyBody ...
 	ErrEmptyBody = errors.New("Empty body")
+	// ErrAPIError ...
+	ErrAPIError = errors.New("Api error")
 )
-
-// Advice ..
-type Advice struct {
-	ID    int64  `json:"id"`
-	Text  string `json:"text"`
-	Sound string `json:"sound"`
-}
 
 // GetRandomAdvice ...
 func GetRandomAdvice() (*Advice, error) {
 	endPnt := fmt.Sprintf("%s%s", apiURL, randomMth)
+
 	return getAdvice(endPnt)
 }
 
 // GetRandomHerAdvice ...
 func GetRandomHerAdvice() (*Advice, error) {
-	endPnt := fmt.Sprintf("%s%s", apiURL, randomHerMth)
-	return getAdvice(endPnt)
+	endPnt := fmt.Sprintf("%s%s%s", apiV2URL, randomTagMth, forHerTag)
+
+	return getAdviceV2(endPnt)
 }
 
 // GetRandomCoderAdvice ...
 func GetRandomCoderAdvice() (*Advice, error) {
-	endPnt := fmt.Sprintf("%s%s", apiURL, randomCoderMth)
-	return getAdvice(endPnt)
+	endPnt := fmt.Sprintf("%s%s%s", apiV2URL, randomTagMth, codingTag)
+
+	return getAdviceV2(endPnt)
 }
 
 func getAdvice(endPnt string) (*Advice, error) {
-	r, err := http.Get(endPnt)
+	body, err := getBody(endPnt)
 	if err != nil {
-		log.Printf("[Warning] can not get random advice, err: %s\n", err)
+		// all errors was processed in getBody
 		return nil, err
 	}
-	if r.StatusCode >= 400 {
-		log.Printf("[Warning] bad status: %d\n", r.StatusCode)
-		return nil, ErrBadStatus
-	}
-	body := r.Body
-	if body == nil {
-		log.Printf("[Warning] nil body\n")
-		return nil, ErrEmptyBody
-	}
+
 	defer body.Close()
+
 	advBytes, err := ioutil.ReadAll(body)
 	if err != nil {
 		log.Printf("[Warning] some problems when read body, err: %s\n", err)
+
 		return nil, err
 	}
 	var adv Advice
 	err = json.Unmarshal(advBytes, &adv)
 	if err != nil {
 		log.Printf("[Warning] some problems when unmarshal json, err: %s\n", err)
+
 		return nil, err
 	}
 	adv.Text = html.UnescapeString(adv.Text)
+
 	return &adv, nil
+}
+
+func getAdviceV2(endPnt string) (*Advice, error) {
+	body, err := getBody(endPnt)
+	if err != nil {
+		// all errors was processed in getBody
+		return nil, err
+	}
+
+	defer body.Close()
+
+	advBytes, err := ioutil.ReadAll(body)
+	if err != nil {
+		log.Printf("[Warning] some problems when read body, err: %s\n", err)
+
+		return nil, err
+	}
+	var advResp AdviceResponse
+	err = json.Unmarshal(advBytes, &advResp)
+	if err != nil {
+		log.Printf("[Warning] some problems when unmarshal json, err: %s\n", err)
+
+		return nil, err
+	}
+	if len(advResp.Errors) != 0 {
+		log.Printf("[Warning] request has errors: %v\n", advResp.Errors)
+
+		return nil, ErrAPIError
+	}
+	if len(advResp.Data) == 0 {
+		log.Printf("[Warning] request has no data\n")
+
+		return nil, ErrAPIError
+	}
+	adv := Advice{}
+	adv.Text = html.UnescapeString(advResp.Data[0].Text)
+
+	return &adv, nil
+}
+
+func getBody(endPnt string) (io.ReadCloser, error) {
+	r, err := http.Get(endPnt)
+	if err != nil {
+		log.Printf("[Warning] can not get random advice, err: %s\n", err)
+
+		return nil, err
+	}
+	if r.StatusCode >= 400 {
+		log.Printf("[Warning] bad status: %d\n", r.StatusCode)
+
+		return nil, ErrBadStatus
+	}
+	body := r.Body
+	if body == nil {
+		log.Printf("[Warning] nil body\n")
+
+		return nil, ErrEmptyBody
+	}
+
+	return body, nil
 }
---
+++ fgaapi/types.go
@@ -0,0 +1,24 @@
+package fgaapi
+
+// AdviceResponse represents response wrapper
+type AdviceResponse struct {
+	Status string   `json:"status"`
+	Errors []string `json:"errors"`
+	Data   []Data   `json:"data"`
+}
+
+// Data represents advice
+type Data struct {
+	ID          int        `json:"id"`
+	Text        string     `json:"text"`
+	HTML        string     `json:"html"`
+	Tags        []string   `json:"tags"`
+	Conclusions []struct{} `json:"conclusions"`
+}
+
+// Advice uses in api v1
+type Advice struct {
+	ID    int64  `json:"id"`
+	Text  string `json:"text"`
+	Sound string `json:"sound"`
+}
